04994e2a6e81a207b2a9c05676315e1a3b3e5e21
mykola <webmintua@gmail.com>
2023-03-30 09:19:31 +0300

    added preserve modules

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
     rollupOptions: {
       external: ["vue"],
       output: {
+        preserveModules: true,
         globals: {
           vue: "Vue",
         },
